59efac74fa774d660922a35917c89cbc09634379
Pavlos <msf7415012@teilar.gr>
2018-07-17 23:13:32 +0300

    feat: customize widget
    Resolves: Exercise 1 - Add New Widget

--- app/src/main/java/com/example/android/mygarden/PlanWidgetProvider.java
+++ app/src/main/java/com/example/android/mygarden/PlanWidgetProvider.java
@@ -1,9 +1,13 @@
 package com.example.android.mygarden;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
 import android.widget.RemoteViews;
+
+import com.example.android.mygarden.ui.MainActivity;
 
 /**
  * Implementation of App Widget functionality.
@@ -16,7 +20,12 @@ public class PlanWidgetProvider extends AppWidgetProvider {
         CharSequence widgetText = context.getString(R.string.appwidget_text);
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.plan_widget_provider);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
+        //views.setTextViewText(R.id.appwidget_text, widgetText);
+
+        Intent intent = new Intent(context, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+        views.setOnClickPendingIntent(R.id.widget_plant_image, pendingIntent);
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
